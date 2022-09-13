e2c9f8a294e2d7f7befafe6203b790f24ca67ab9
Unknown <adtsolanki22@gmail.com>
2022-09-13 11:30:15 +0530

    Started with core pages

---
+++ pages/posts/[slug].js
@@ -0,0 +1,9 @@
+// /posts/getting-started-with-nextjs
+function PostDetailPage() {
+   return (
+     <div>
+       <p>Post Detail Page</p>
+     </div>
+    )
+ }
+ export default PostDetailPage;
---
+++ pages/posts/index.js
@@ -0,0 +1,9 @@
+// /posts
+function AllPostsPage() {
+   return (
+     <div>
+       <p>All Posts Page</p>
+     </div>
+    )
+ }
+ export default AllPostsPage;

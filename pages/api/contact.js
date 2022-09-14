07f19791726ab072e5a84f64e28155e6b23880c0
Unknown <adtsolanki22@gmail.com>
2022-09-14 13:14:07 +0530

    Connected Contact form with mongodb

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,5 +1,6 @@
 // /api/contact
-function handler(req,res) {
+import { MongoClient } from 'mongodb';
+async function handler(req,res) {
    if(req.method === 'POST') {
       const { email, name, message } = req.body;
       if(!email ||
@@ -19,6 +20,25 @@ function handler(req,res) {
          message
       };
       console.log('new msg',newMessage);
+      let client;
+      try {
+         const MONGO_URI = `mongodb+srv://aditi:NruNqUTV1OPMxTvN@cluster0.v364j.mongodb.net/blogApp?retryWrites=true&w=majority`;
+         client = await MongoClient.connect(MONGO_URI);
+      
+      } catch(e) {
+         res.status(500).json({ message: "Failed to connect with Database", message: newMessage });
+         return;
+      }
+      const db = client.db();
+      try {
+         const result = await db.collection('messages').insertOne(newMessage);
+         newMessage.id = result.insertedId;
+      } catch(e) {
+         client.close();
+         res.status(500).json({ message: "storing message failed" });
+         return;
+      }
+      client.close();
       res.status(201).json({ message: "successfully store message", message: newMessage });
    }
 }

87af844ba6057606cefa71663ca84a14d6b2786c
Unknown <adtsolanki22@gmail.com>
2022-09-14 20:36:14 +0530

    Fixed error of atom

--- components/post-detail/post-content.js
+++ components/post-detail/post-content.js
@@ -1,8 +1,7 @@
 import ReactMarkdown from 'react-markdown';
 import Image from 'next/image';
-//use prism light version
 import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
-import atomDark from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import atomDark from 'react-syntax-highlighter/dist/cjs/styles/prism/atom-dark';
 import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
 import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css';
 
@@ -55,9 +54,9 @@ function PostContent(props) {
         <SyntaxHighlighter
           style={atomDark}
           language={language}
-          // children={value}
+         // children={value}
         >
-        {value}
+          {value}
         </SyntaxHighlighter>
       );
     },
